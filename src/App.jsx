4fe2e64806044a78b726fed59fa1cb11aff46a9d
Nkugavarathan <kugavarathan28@gmail.com>
2025-03-02 23:20:13 +0530

    animation checked

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 import { AnimatedBackground } from "animated-backgrounds"
-
+import Header from "./components/Header"
 export default function App() {
   return (
     <div>
+      <AnimatedBackground animationName="quantumField" blendMode="screen" />
+      {/* Basic usage */}
+
       <Navbar />
+      <Header />
     </div>
   )
 }
