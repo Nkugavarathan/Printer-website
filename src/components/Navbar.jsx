ac173a65b5d32564116cce38a623eee8e612eb28
Nkugavarathan <kugavarathan28@gmail.com>
2025-03-02 22:18:34 +0530

    navbar started

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react"
+import Container from "react-bootstrap/Container"
+import Nav from "react-bootstrap/Nav"
+import Navbar from "react-bootstrap/Navbar" // Un-commented this line
+
+export default function CustomNavbar() {
+  const [scrolled, setScrolled] = useState(false)
+
+  // Handle the scroll event and change the navbar appearance
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 30) {
+        setScrolled(true)
+      } else {
+        setScrolled(false)
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll)
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+
+  return (
+    <Navbar
+      className={`navbar ${scrolled ? "navbar-sticky bg-dark" : ""}`}
+      expand="lg"
+    >
+      <Container fluid>
+        <Navbar.Brand href="#">Redux Toolkit</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="me-auto my-2 my-lg-0">
+            <Nav.Link href="#">Products</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+
+        <Navbar.Collapse className="justify-content-end me-2">
+          <Nav>
+            <Nav.Link href="#">Contact</Nav.Link>
+            <Nav.Link href="#">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
